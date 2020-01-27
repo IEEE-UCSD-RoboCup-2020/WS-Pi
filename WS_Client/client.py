da2001046d5ffa8d0224d5fb2bc26748c180861b
ReSummit <star9123.dv@gmail.com>
2020-01-26 16:47:26 -0800

    Added implementation of connecting and maintaining list of connected clients.

--- WS_Client/client.py
+++ WS_Client/client.py
@@ -3,14 +3,17 @@
 import asyncio
 import websockets
 
+# Just waits for a response (or just how many users are there.
 async def hello():
-    uri = "ws://(broadcasted IP to connect to):8765"
+    uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        message = input()
-        await websocket.send(message)
-        response = await websocket.recv()
-        if (response != ""):
-            print(response)
+        await websocket.send("Connect.");
+        await websocket.recv();
+        while(True):
+            await websocket.send("")
+            response = await websocket.recv()
+            if (response != ""):
+                print(response)
 
 while(True):
     asyncio.get_event_loop().run_until_complete(hello())
--- WS_Server/server.py
+++ WS_Server/server.py
@@ -4,14 +4,36 @@ import time
 import asyncio
 import websockets
 
-async def echo(websocket, path):
-    async for message in websocket:
-        received = message
-        print(message)
-        received = time.time()
-        await websocket.send("Whoops")
+USERS = set()
 
-start_server = websockets.serve(echo, "(broadcasted IP)", 8765)
+# Methods handling registration of new clients
+async def register(websocket):
+    USERS.add(websocket)
+    await asyncio.wait([user.send("Device was connected.") for user in USERS])
+
+async def unregister(websocket):
+    USERS.remove(websocket)
+    await asyncio.wait([user.send("Device was disconnected.") for user in USERS])
+
+# Main method sending number of clients to all users (currently number of users)
+async def main(websocket, path):
+    await register(websocket)
+    try:
+        async for message in websocket:
+            received = message
+            if (message != ""):
+                print(message)
+            await websocket.send(str(len(USERS)))
+    finally:
+        await unregister(websocket)
+
+# TODO: Make commands while sending and receiving messages
+async def command():
+    while(True):
+        command = input()
+        print(command)
+
+start_server = websockets.serve(main, "localhost", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
